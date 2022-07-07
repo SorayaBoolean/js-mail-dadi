107148f61b8334e3d589bfb7f9ce85a759815596
SorayaBoolean <sorayakhyabaniassl@gmail.com>
2022-07-07 17:59:20 +0200

    First step finished

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const emailAddress = prompt ('Inserisci la tua email');
 console.log (emailAddress)
 
-const listaEmails = [ 'soraya@boolean.it', 'giovanni@boolean.it', 'marco@boolean.it', 'sara@boolean.it', 'michela@boolean.it', 'jacopo@boolean.it'];
-console.log (listaEmails)
+const listEmails = [ 'soraya@boolean.it', 'giovanni@boolean.it', 'marco@boolean.it', 'sara@boolean.it', 'michela@boolean.it', 'jacopo@boolean.it'];
+console.log (listEmails)
+
+let enterAllowed = 'Nope'
 
 for (let i= 0; i< 6; i++ ) {
-    console.log (i, listaEmails [i]);
+    console.log (listEmails [i]);
+
+    if (emailAddress == listEmails [i]) {
+        let enterAllowed = 'Allowed'
+        console.log (enterAllowed);
+        alert (emailAddress + ' sei entrato')
+    }
+}
+
+if (enterAllowed != 'Allowed') {
+    console.log ('You are note Allowed')
+    alert (emailAddress + ' non sei entrato')
 }

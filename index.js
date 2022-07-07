256ac60a6e06fa2eb8a88fb405d150fbcab89896
SorayaBoolean <sorayakhyabaniassl@gmail.com>
2022-07-07 17:32:45 +0200

    start: added array and first cicle

---
+++ index.js
@@ -0,0 +1,9 @@
+const emailAddress = prompt ('Inserisci la tua email');
+console.log (emailAddress)
+
+const listaEmails = [ 'soraya@boolean.it', 'giovanni@boolean.it', 'marco@boolean.it', 'sara@boolean.it', 'michela@boolean.it', 'jacopo@boolean.it'];
+console.log (listaEmails)
+
+for (let i= 0; i< 6; i++ ) {
+    console.log (i, listaEmails [i]);
+}

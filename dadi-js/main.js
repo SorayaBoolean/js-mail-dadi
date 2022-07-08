656138e8f04c1efddd8f29aa5a0b680b60ed411e
SorayaBoolean <sorayakhyabaniassl@gmail.com>
2022-07-08 10:48:35 +0200

    going to fix

--- dadi-js/main.js
+++ dadi-js/main.js
@@ -1,26 +1,9 @@
-const numeroGiocatore = parseInt (prompt ('Scegli un numero da 1 a 6'));
+const numeroGiocatore = Math.floor (Math.random ()*6) + 1;
 console.log (numeroGiocatore)
-
-const numeroComputer = Math.floor (Math.random ()*5) + 1;
+const numeroComputer = Math.floor (Math.random ()*6) + 1;
 console.log (numeroComputer)
 
-const listaComputer = ['1', '2', '3', '4', '5', '6' ]
-console.log (listaComputer)
 
-let Vinto = 'Computer'
-
-for ( let i = 0; i < 6 ; i++) {
-
-if (numeroGiocatore > numeroComputer) {
-    let Vinto = 'Tu'
-    console.log (Vinto)
-    alert ('Hai vinto tu!')
-} 
-}
-
-if (Vinto != 'Tu') {
-    alert ('Avete pareggiato!')
-}
 
 
 
